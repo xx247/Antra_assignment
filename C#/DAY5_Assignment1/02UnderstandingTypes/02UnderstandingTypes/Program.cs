00048124772f706edca5b7326c9f8ba5357ffebe
xx247 <xx97247@gmail.com>
2024-06-25 13:54:12 -0400

    Add files via upload

---
+++ C#/DAY5_Assignment1/02UnderstandingTypes/02UnderstandingTypes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _02UnderstandingTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type: {0, -10} Size: {1, -5} Min: {2, -30} Max: {3}",
+                "sbyte", sizeof(sbyte), sbyte.MinValue, sbyte.MaxValue);
+            Console.WriteLine("Type: {0, -10} Size: {1, -5} Min: {2, -30} Max: {3}",
+                "byte", sizeof(byte), byte.MinValue, byte.MaxValue);
+            Console.WriteLine("Type: {0, -10} Size: {1, -5} Min: {2, -30} Max: {3}",
+                "short", sizeof(short), short.MinValue, short.MaxValue);
+            Console.WriteLine("Type: {0, -10} Size: {1, -5} Min: {2, -30} Max: {3}",
+                "ushort", sizeof(ushort), ushort.MinValue, ushort.MaxValue);
+            Console.WriteLine("Type: {0, -10} Size: {1, -5} Min: {2, -30} Max: {3}",
+                "int", sizeof(int), int.MinValue, int.MaxValue);
+            Console.WriteLine("Type: {0, -10} Size: {1, -5} Min: {2, -30} Max: {3}",
+                "uint", sizeof(uint), uint.MinValue, uint.MaxValue);
+            Console.WriteLine("Type: {0, -10} Size: {1, -5} Min: {2, -30} Max: {3}",
+                "long", sizeof(long), long.MinValue, long.MaxValue);
+            Console.WriteLine("Type: {0, -10} Size: {1, -5} Min: {2, -30} Max: {3}",
+                "ulong", sizeof(ulong), ulong.MinValue, ulong.MaxValue);
+            Console.WriteLine("Type: {0, -10} Size: {1, -5} Min: {2, -30} Max: {3}",
+                "float", sizeof(float), float.MinValue, float.MaxValue);
+            Console.WriteLine("Type: {0, -10} Size: {1, -5} Min: {2, -30} Max: {3}",
+                "double", sizeof(double), double.MinValue, double.MaxValue);
+            Console.WriteLine("Type: {0, -10} Size: {1, -5} Min: {2, -30} Max: {3}",
+                "decimal", sizeof(decimal), decimal.MinValue, decimal.MaxValue);
+        }
+    }
+}
